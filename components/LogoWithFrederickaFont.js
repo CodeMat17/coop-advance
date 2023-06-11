e71ccec8d3df5bc7348c4372bf8fc1fd35541405
CodeMat <codemat.biz@gmail.com>
2023-06-11 15:48:09 +0100

    update

--- components/LogoWithFrederickaFont.js
+++ components/LogoWithFrederickaFont.js
@@ -1,5 +1,6 @@
 import { Box, Text } from "@chakra-ui/react";
 import Image from "next/image";
+import { fredericka } from "./FrederickaFont";
 
 const LogoWithFrederikaFont = () => {
   return (
@@ -7,11 +8,14 @@ const LogoWithFrederikaFont = () => {
       <Box display='flex' alignItems='center'>
        <Box rounded='full'>
           <Image alt='' width={80} height={0} src='/logo.webp' />
-         </Box>
-        <Box fontWeight='semibold' lineHeight='none' bgClip='text' bgGradient='linear(to-tl, #ec6d25, #086daf)' >
+        </Box>
+        <div className={fredericka.className}>
+          <Box fontWeight='semibold' lineHeight='none' bgClip='text' bgGradient='linear(to-tl, #ec6d25, #086daf)' >
           <Text>COOP</Text>
           <Text>Advance</Text>
         </Box>
+        </div>
+        
       </Box>
     </>
   );
