0853cb76b8a3a8778c077d9ae9008dda24b74832
CodeMat <codemat.biz@gmail.com>
2023-06-11 15:50:20 +0100

    updated

--- components/LogoWithFrederickaFont.js
+++ components/LogoWithFrederickaFont.js
@@ -9,12 +9,12 @@ const LogoWithFrederikaFont = () => {
        <Box rounded='full'>
           <Image alt='' width={80} height={0} src='/logo.webp' />
         </Box>
-        <div className={fredericka.className}>
+        {/* <div className={fredericka.className}> */}
           <Box fontWeight='semibold' lineHeight='none' bgClip='text' bgGradient='linear(to-tl, #ec6d25, #086daf)' >
           <Text>COOP</Text>
           <Text>Advance</Text>
         </Box>
-        </div>
+        {/* </div> */}
         
       </Box>
     </>
